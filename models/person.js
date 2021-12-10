bc0917aa2d3627988517da0c225d1bcbbeb8af7b
hieunv0104 <vanhieu@eplaza.vn>
2021-12-10 20:35:50 +0700

    update code

--- models/person.js
+++
@@ -1,9 +0,0 @@
-class Person{
-    constructor(data) {
-        this.codes = data.codes;
-        this.vehicle_type = data.vehicle_type;
-        this.information_province = data.information_province;
-    }
-
-}
-module.exports = Person;
