3114da2c97c478c169e4e96e725bbb3dec4c499a
Diennv <diennv@ttx-solutions.com.vn>
2021-12-17 18:59:18 +0700

    a

--- src/apiClient/routes/login.js
+++ src/apiClient/routes/login.js
@@ -11,7 +11,7 @@ router.post("/login", controller.login);
 
 router.post("/register", controller.register);
 
-router.post("/save-firebase-token", controller.saveFirebaseToken);
+router.post("/save-firebase-token", (req,res)=>controller.saveFirebaseToken(req,res));
 
 
 module.exports = router;
