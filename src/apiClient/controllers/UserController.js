2cc838922fe41b31c12ea66126429545c715dcde
Diennv <diennv@ttx-solutions.com.vn>
2021-12-17 14:53:09 +0700

    a

--- src/apiClient/controllers/UserController.js
+++ src/apiClient/controllers/UserController.js
@@ -8,7 +8,7 @@ class UserController {
         User.setInfoUser(req.con, req.body, (err, ress) => {
             if (err) return res.status(503).json(errorResponse(503, 'Update user error', err));
             if (ress.affectedRows === 0) return res.status(403).json(errorResponse(403, 'Not Found'));
-            return res.status(200).json(successResponse(200, 'OK'));
+            return res.status(200).json(successResponse(200, req.body));
         })
     }
 
